eb0fb99a63bbde1457ae1fa0ce3211f9435121f3
Aman Sharma <mannu.poski10@gmail.com>
2019-08-28 11:10:43 +0530

    Create gist through export modal

--- src/components/header/export-modal/renderer.tsx
+++ src/components/header/export-modal/renderer.tsx
@@ -1,15 +1,19 @@
 import * as React from 'react';
-import {Book, Code, Image, Map} from 'react-feather';
+import {Book, Code, Image, Map, GitHub} from 'react-feather';
 import {withRouter} from 'react-router-dom';
 import {mergeDeep} from 'vega-lite/build/src/util';
 import {mapStateToProps} from '.';
-import {Mode} from '../../../constants/consts';
+import {Mode, BACKEND_URL, COOKIE_NAME} from '../../../constants/consts';
 import './index.css';
+import getCookie from '../../../utils/getCookie';
 
 type Props = ReturnType<typeof mapStateToProps>;
 
 interface State {
   downloadVegaJSON: boolean;
+  gistFileName: string;
+  gistPrivate: boolean;
+  gistTitle: string;
   includeConfig: boolean;
 }
 
@@ -18,6 +22,9 @@ class ExportModal extends React.PureComponent<Props, State> {
     super(props);
     this.state = {
       downloadVegaJSON: false,
+      gistFileName: '',
+      gistPrivate: false,
+      gistTitle: '',
       includeConfig: true
     };
   }
@@ -118,6 +125,47 @@ class ExportModal extends React.PureComponent<Props, State> {
     this.setState({downloadVegaJSON: event.currentTarget.value === 'vega'});
   }
 
+  public updatePrivacy(event) {
+    this.setState({
+      gistPrivate: event.target.checked
+    });
+  }
+
+  public fileNameChange(event) {
+    this.setState({
+      gistFileName: event.target.value
+    });
+  }
+
+  public titleChange(event) {
+    this.setState({
+      gistTitle: event.target.value
+    });
+  }
+
+  public createGist() {
+    const body = {
+      content: this.props.editorString,
+      name: this.state.gistFileName,
+      title: this.state.gistTitle,
+      privacy: this.state.gistPrivate
+    };
+    console.log(this.props.editorString);
+    const cookieValue = encodeURIComponent(getCookie(COOKIE_NAME));
+    fetch(`${BACKEND_URL}gists/create`, {
+      body: JSON.stringify(body),
+      mode: 'cors',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+        Cookie: `${COOKIE_NAME}=${cookieValue}`
+      },
+      method: 'post'
+    })
+      .then(res => res.json())
+      .then(json => console.log(json));
+  }
+
   public render() {
     return (
       <>
@@ -205,7 +253,7 @@ class ExportModal extends React.PureComponent<Props, State> {
             <button onClick={() => this.openViz('svg')} className="export-button">
               Open
             </button>
-            <button onClick={() => this.downloadViz('svg')} className="export-button download">
+            <button onClick={() => this.downloadViz('svg')} className="export-button two">
               Download
             </button>
           </div>
@@ -221,6 +269,51 @@ class ExportModal extends React.PureComponent<Props, State> {
             </p>
             <button onClick={() => this.downloadPDF()} className="export-button">
               Download
+            </button>
+          </div>
+          <div className="export-button-container">
+            <div className="header-text">
+              <GitHub />
+              <span>GIST</span>
+            </div>
+            <p>Create a GitHub gist directly using the editor.</p>
+            <div className="input-container">
+              <div>
+                <input
+                  type="checkbox"
+                  name="private-gist"
+                  id="private-gist"
+                  value="private-select"
+                  checked={this.state.gistPrivate}
+                  onChange={this.updatePrivacy.bind(this)}
+                />
+                <label htmlFor="private-gist">Private gist</label>
+              </div>
+            </div>
+            <div className="input-container flex">
+              <label>
+                File name:
+                <input
+                  value={this.state.gistFileName}
+                  onChange={this.fileNameChange.bind(this)}
+                  className="export-button create"
+                  type="text"
+                  placeholder="Enter file name"
+                />
+              </label>
+              <label>
+                Title (optional):
+                <input
+                  value={this.state.gistTitle}
+                  onChange={this.titleChange.bind(this)}
+                  className="export-button create"
+                  type="text"
+                  placeholder="Enter title of gist (optional)"
+                />
+              </label>
+            </div>
+            <button className="export-button two" onClick={this.createGist.bind(this)}>
+              Create
             </button>
           </div>
         </div>
