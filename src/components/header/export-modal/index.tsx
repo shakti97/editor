aae1674b0ce2d4efc2dec4f6b9b389c9d287cc7b
Aman Sharma <mannu.poski10@gmail.com>
2019-08-28 14:55:05 +0530

    Move create gist option to share modal

--- src/components/header/export-modal/index.tsx
+++ src/components/header/export-modal/index.tsx
@@ -1,11 +1,10 @@
-import {connect} from 'react-redux';
-import {State} from '../../../constants/default-state';
-import Renderer from './renderer';
+import { connect } from "react-redux";
+import { State } from "../../../constants/default-state";
+import Renderer from "./renderer";
 
 export function mapStateToProps(state: State) {
   return {
     config: state.config,
-    editorString: state.editorString,
     mode: state.mode,
     vegaLiteSpec: state.vegaLiteSpec,
     vegaSpec: state.vegaSpec,
--- src/components/header/export-modal/renderer.tsx
+++ src/components/header/export-modal/renderer.tsx
@@ -1,19 +1,15 @@
-import * as React from 'react';
-import {Book, Code, Image, Map, GitHub} from 'react-feather';
-import {withRouter} from 'react-router-dom';
-import {mergeDeep} from 'vega-lite/build/src/util';
-import {mapStateToProps} from '.';
-import {Mode, BACKEND_URL, COOKIE_NAME} from '../../../constants/consts';
-import './index.css';
-import getCookie from '../../../utils/getCookie';
+import * as React from "react";
+import { Book, Code, Image, Map, GitHub } from "react-feather";
+import { withRouter } from "react-router-dom";
+import { mergeDeep } from "vega-lite/build/src/util";
+import { mapStateToProps } from ".";
+import { Mode } from "../../../constants/consts";
+import "./index.css";
 
 type Props = ReturnType<typeof mapStateToProps>;
 
 interface State {
   downloadVegaJSON: boolean;
-  gistFileName: string;
-  gistPrivate: boolean;
-  gistTitle: string;
   includeConfig: boolean;
 }
 
@@ -22,25 +18,22 @@ class ExportModal extends React.PureComponent<Props, State> {
     super(props);
     this.state = {
       downloadVegaJSON: false,
-      gistFileName: '',
-      gistPrivate: false,
-      gistTitle: '',
       includeConfig: true
     };
   }
 
   public async downloadViz(ext: string) {
     const url = await this.props.view.toImageURL(ext);
-    const link = document.createElement('a');
-    link.setAttribute('href', url);
-    link.setAttribute('target', '_blank');
-    link.setAttribute('download', `visualization.${ext}`);
-    link.dispatchEvent(new MouseEvent('click'));
+    const link = document.createElement("a");
+    link.setAttribute("href", url);
+    link.setAttribute("target", "_blank");
+    link.setAttribute("download", `visualization.${ext}`);
+    link.dispatchEvent(new MouseEvent("click"));
   }
 
   public async openViz(ext: string) {
     const url = await this.props.view.toImageURL(ext);
-    const tab = window.open('about:blank', '_blank');
+    const tab = window.open("about:blank", "_blank");
     tab.document.write(`<title>Chart</title><img src="${url}" />`);
     tab.document.close();
   }
@@ -48,35 +41,36 @@ class ExportModal extends React.PureComponent<Props, State> {
   public async downloadPDF() {
     // show that we are working
     const dlButton = this.refs.downloadPDF as any;
-    dlButton.classList.add('disabled');
+    dlButton.classList.add("disabled");
 
     const svg = await this.props.view.toSVG();
 
-    const pdf = await fetch('https://api.cloudconvert.com/convert', {
+    const pdf = await fetch("https://api.cloudconvert.com/convert", {
       body: JSON.stringify({
-        apikey: '7ZSKlPLjDB4RUaq5dvEvAQMG5GGwEeHH3qa7ixAr0KZtPxfwsKv81sc1SqFhlh7d',
+        apikey:
+          "7ZSKlPLjDB4RUaq5dvEvAQMG5GGwEeHH3qa7ixAr0KZtPxfwsKv81sc1SqFhlh7d",
         file: svg,
-        filename: 'chart.svg',
-        input: 'raw',
-        inputformat: 'svg',
-        outputformat: 'pdf'
+        filename: "chart.svg",
+        input: "raw",
+        inputformat: "svg",
+        outputformat: "pdf"
       }),
       headers: {
-        'content-type': 'application/json; chartset=UTF-8'
+        "content-type": "application/json; chartset=UTF-8"
       },
-      method: 'post'
+      method: "post"
     });
 
     const blob = await pdf.blob();
     const url = window.URL.createObjectURL(blob);
 
-    const link = document.createElement('a');
-    link.setAttribute('href', url);
-    link.setAttribute('target', '_blank');
-    link.setAttribute('download', `visualization.pdf`);
-    link.dispatchEvent(new MouseEvent('click'));
+    const link = document.createElement("a");
+    link.setAttribute("href", url);
+    link.setAttribute("target", "_blank");
+    link.setAttribute("download", `visualization.pdf`);
+    link.dispatchEvent(new MouseEvent("click"));
 
-    dlButton.classList.remove('disabled');
+    dlButton.classList.remove("disabled");
   }
 
   public updateIncludeConfig(e) {
@@ -100,12 +94,16 @@ class ExportModal extends React.PureComponent<Props, State> {
       content = this.props.vegaSpec;
       filename = `visualization.vg.json`;
     } else {
-      content = this.state.downloadVegaJSON ? this.props.vegaSpec : this.props.vegaLiteSpec;
-      filename = this.state.downloadVegaJSON ? `visualization.vg.json` : `visualization.vl.json`;
+      content = this.state.downloadVegaJSON
+        ? this.props.vegaSpec
+        : this.props.vegaLiteSpec;
+      filename = this.state.downloadVegaJSON
+        ? `visualization.vg.json`
+        : `visualization.vl.json`;
     }
 
     if (this.state.includeConfig && this.props.config) {
-      content = {...content};
+      content = { ...content };
       content.config = mergeDeep({}, this.props.config, content.config);
     }
 
@@ -122,48 +120,7 @@ class ExportModal extends React.PureComponent<Props, State> {
   }
 
   public updateDownloadJSONType(event) {
-    this.setState({downloadVegaJSON: event.currentTarget.value === 'vega'});
-  }
-
-  public updatePrivacy(event) {
-    this.setState({
-      gistPrivate: event.target.checked
-    });
-  }
-
-  public fileNameChange(event) {
-    this.setState({
-      gistFileName: event.target.value
-    });
-  }
-
-  public titleChange(event) {
-    this.setState({
-      gistTitle: event.target.value
-    });
-  }
-
-  public createGist() {
-    const body = {
-      content: this.props.editorString,
-      name: this.state.gistFileName,
-      title: this.state.gistTitle,
-      privacy: this.state.gistPrivate
-    };
-    console.log(this.props.editorString);
-    const cookieValue = encodeURIComponent(getCookie(COOKIE_NAME));
-    fetch(`${BACKEND_URL}gists/create`, {
-      body: JSON.stringify(body),
-      mode: 'cors',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-        Cookie: `${COOKIE_NAME}=${cookieValue}`
-      },
-      method: 'post'
-    })
-      .then(res => res.json())
-      .then(json => console.log(json));
+    this.setState({ downloadVegaJSON: event.currentTarget.value === "vega" });
   }
 
   public render() {
@@ -177,11 +134,14 @@ class ExportModal extends React.PureComponent<Props, State> {
               <span>PNG</span>
             </div>
             <p>
-              PNG is a bitmap image format which is made up of a fixed number of pixels. They have a fixed resolution
-              and cannot be scaled.
+              PNG is a bitmap image format which is made up of a fixed number of
+              pixels. They have a fixed resolution and cannot be scaled.
             </p>
-            <button onClick={() => this.downloadViz('png')} className="export-button">
-              Download{' '}
+            <button
+              onClick={() => this.downloadViz("png")}
+              className="export-button"
+            >
+              Download{" "}
             </button>
           </div>
 
@@ -201,7 +161,7 @@ class ExportModal extends React.PureComponent<Props, State> {
                     value="vega"
                     checked={this.state.downloadVegaJSON}
                     onChange={this.updateDownloadJSONType.bind(this)}
-                  />{' '}
+                  />{" "}
                   Compiled Vega
                 </label>
                 <label htmlFor="json-type[vega-lite]" className="vl-label">
@@ -233,11 +193,16 @@ class ExportModal extends React.PureComponent<Props, State> {
                     />
                     Include config
                   </label>
-                  {this.state.includeConfig && <p>The downloaded spec will be formatted. </p>}
+                  {this.state.includeConfig && (
+                    <p>The downloaded spec will be formatted. </p>
+                  )}
                 </div>
               )}
             </div>
-            <button onClick={e => this.downloadJSON(e)} className="export-button">
+            <button
+              onClick={e => this.downloadJSON(e)}
+              className="export-button"
+            >
               Download
             </button>
           </div>
@@ -247,13 +212,20 @@ class ExportModal extends React.PureComponent<Props, State> {
               <span>SVG</span>
             </div>
             <p>
-              SVG is a vector image format which uses geometric forms to represent different parts as discrete objects
-              and are infinitely scalable.
+              SVG is a vector image format which uses geometric forms to
+              represent different parts as discrete objects and are infinitely
+              scalable.
             </p>
-            <button onClick={() => this.openViz('svg')} className="export-button">
+            <button
+              onClick={() => this.openViz("svg")}
+              className="export-button"
+            >
               Open
             </button>
-            <button onClick={() => this.downloadViz('svg')} className="export-button two">
+            <button
+              onClick={() => this.downloadViz("svg")}
+              className="export-button two"
+            >
               Download
             </button>
           </div>
@@ -264,62 +236,23 @@ class ExportModal extends React.PureComponent<Props, State> {
             </div>
             <p>
               <strong>Experimental!</strong>
-              <br /> PDF is a vector format usually used for documents. This might take a few seconds. Please be
-              patient. Your chart is sent to an <a href="https://cloudconvert.com/">external service</a> for processing.
+              <br /> PDF is a vector format usually used for documents. This
+              might take a few seconds. Please be patient. Your chart is sent to
+              an <a href="https://cloudconvert.com/">external service</a> for
+              processing.
             </p>
-            <button onClick={() => this.downloadPDF()} className="export-button">
+            <button
+              onClick={() => this.downloadPDF()}
+              className="export-button"
+            >
               Download
-            </button>
-          </div>
-          <div className="export-button-container">
-            <div className="header-text">
-              <GitHub />
-              <span>GIST</span>
-            </div>
-            <p>Create a GitHub gist directly using the editor.</p>
-            <div className="input-container">
-              <div>
-                <input
-                  type="checkbox"
-                  name="private-gist"
-                  id="private-gist"
-                  value="private-select"
-                  checked={this.state.gistPrivate}
-                  onChange={this.updatePrivacy.bind(this)}
-                />
-                <label htmlFor="private-gist">Private gist</label>
-              </div>
-            </div>
-            <div className="input-container flex">
-              <label>
-                File name:
-                <input
-                  value={this.state.gistFileName}
-                  onChange={this.fileNameChange.bind(this)}
-                  className="export-button create"
-                  type="text"
-                  placeholder="Enter file name"
-                />
-              </label>
-              <label>
-                Title (optional):
-                <input
-                  value={this.state.gistTitle}
-                  onChange={this.titleChange.bind(this)}
-                  className="export-button create"
-                  type="text"
-                  placeholder="Enter title of gist (optional)"
-                />
-              </label>
-            </div>
-            <button className="export-button two" onClick={this.createGist.bind(this)}>
-              Create
             </button>
           </div>
         </div>
         <div className="user-notes">
           <p>
-            <strong>Note:</strong> To get a PDF, open the SVG which you can print as a PDF from your browser.
+            <strong>Note:</strong> To get a PDF, open the SVG which you can
+            print as a PDF from your browser.
           </p>
         </div>
       </>
